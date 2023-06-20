f2aacf7c0871d95a610a8bb0c900792817cb2114
mamianeji <mamia.neji@gmail.com>
2023-06-20 22:41:16 +0200

    first commit

---
+++ routes/user.js
@@ -0,0 +1,18 @@
+const express=require("express");
+
+const userRouter=express.Router();
+
+
+
+userRouter.get("/store", async(req,res)=> {
+    const date= new Date();
+    const hours= date.getHours();
+    if(hours>8 && hours<18){
+        res.render("opened")
+    }else{
+        res.render("closed")
+    }
+});
+
+
+module.exports=userRouter;
